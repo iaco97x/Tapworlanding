0139e4b1b71be3dd4328008ba5cbf442416d02f6
iaco97x <iacovelli.f97@gmail.com>
2026-04-22 22:40:41 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
           </div>
           
           <motion.a
-            href="https://testflight.apple.com/join/uahVF1EJ"
+            href="https://apps.apple.com/us/app/tapwork/id6757710879"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ scale: 1.05 }}
